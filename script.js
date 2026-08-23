512bcc643bb171eef35d5d8383aa977c22606beb
Dominik <koslowski.dominik@gmx.de>
2026-08-23 12:19:49 +0200

    Terminformular und Eingabefelder hinzugefügt

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const titelInput = document.getElementById("titel");
+const datumInput = document.getElementById("datum");
+const uhrzeitInput = document.getElementById("uhrzeit");
+const speichernButton = document.getElementById("speichern");
 
+
+
+speichernButton.addEventListener("click", function (){
+    
+const titel = titelInput.value;
+const datum = datumInput.value;
+const uhrzeit = uhrzeitInput.value;
+
+console.log(titel);
+console.log(datum);
+console.log(uhrzeit);
+
+
+
+});
